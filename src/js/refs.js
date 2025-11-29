09b1bd1cc94a15a5545bd822f05b151ee5b49845
Mr_Whoopee <artem.holovko.97@gmail.com>
2025-11-29 15:48:17 +0200

    fix modal, hidden scrollbars

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,14 +1,9 @@
 export const artistListEl = document.querySelector('.artists-list');
 export const artistModalEl = document.querySelector('.modal');
-
+export const backdropWithModalEl = document.querySelector(
+  '.backdrop-with-modal'
+);
 export const artistModalList = document.querySelector('.artist-modal-list');
 export const artistModalPagesEl = document.querySelector('.artists-pages');
 export const searchFormEl = document.querySelector('.filters');
 export const genresListEl = document.querySelector('.js-genres-list');
-export const backdropWithModalEl = document.querySelector('.backdrop-with-modal')
-export const modalCloseButtonEl = document.querySelector('.modal-close-button')
-
-
-
-
-
